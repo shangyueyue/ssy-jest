438a9db7ecc769201848754864655d78f390b178
shangyueyue088 <shangyueyue088@pingan.com.cn>
2019-11-11 19:53:08 +0800

    修改package

--- example/src/app/locales/en-US/editpage/editlist.js
+++
@@ -1,9 +0,0 @@
-export default {
-  "app_editpage_editlist_partyNo": "partyNo",
-  "app_editpage_editlist_userName": "Customer Name",
-  "app_editpage_editlist_mobileNo": "Mobile Number",
-  "app_editpage_editlist_email": "E-mail",
-  "app_editpage_editlist_registerTime": "Registration Date",
-  "app_editpage_editlist_openAccountTime": "Account Opening Date"
-};
-       
