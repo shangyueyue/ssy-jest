e42848fc269d8850248901b00fbb8598fd1808cf
shangyueyue088 <shangyueyue088@pingan.com.cn>
2019-11-13 10:29:24 +0800

    feat(readme): 完善readme

--- README.md
+++ README.md
@@ -1,7 +1,48 @@
-# ice-generate
+## ice-generate
 
-可以通过命令的方式基于接口文档制动生成页面上，生成多语言配置
+### 功能介绍
 
-# 使用方法
+基于接口文档，自动化构建动态页面，生成多语言配置，在此基础上，对数据进行定制化开发，提高开发效率
 
-npx ice-generate -d src/app/pages
+### 使用方法
+
+#### 第一步
+
+在跟目录 generaterc.js 配置：  
+以 table 为例  
+![](assets/images/2019-11-13-10-15-01.png)
+
+#### 第二步
+
+执行命令，既可以生成对应页面
+
+```
+
+\$ npx ice-generate -d src/app/pages
+
+or
+
+$ yarn add ice-generate --dev
+
+"scripts": {
+"ice-generate": "ice-generate -d src/app/pages"
+},
+
+$ npm run ice-generate
+
+```
+### 温馨提示：
+在 locales/en-US/index.js 写入下列代码自动引入该目录下的文件  
+```
+const context = require.context('./', true, /\.js$/);
+const modules = context.keys().reduce((datas, file) => {
+  if (file === './index.js') return datas;
+  const fileDatas = context(file).default;
+  return { ...datas, ...fileDatas };
+}, {});
+export default modules;
+```
+
+### 成果展示：
+
+![](assets/images/2019-11-13-10-21-36.png)
--- example/generaterc.js
+++ example/generaterc.js
@@ -1,45 +1,66 @@
 
+// module.exports = {
+//   res_code: '0',
+//   res_msg: '成功',
+//   data: {
+//     personInfo: {
+//       partyNo: '111',
+//       firstName: '111',
+//       middleName: '111',
+//       lastName: '111',
+//       idType: '111',
+//       idNo: '111',
+//       sex: '111',
+//       birthday: '111',
+//       idExpireDate: '111',
+//       mobileNo: '111',
+//       email: '111',
+//     },
+//     employmentInfo: {
+//       'job ': '111',
+//       company: '111',
+//       fundsource: '111',
+//     },
+//     taxInfo: {
+//       residence: '111',
+//       tin: '111',
+//       lackReason: '111',
+//       explanation: '1111',
+//     },
+//     residential: {
+//       region: '111',
+//       city: '111',
+//       address: '111',
+//       zipCode: '111',
+//       identificationMethod: '111',
+
+//     },
+//     csa: {
+//       riskProfile: '111',
+//     },
+//     bankInfo: {
+//       referenceCode: '111',
+//     },
+//   },
+// };
+
+
 module.exports = {
   res_code: '0',
-  res_msg: '成功',
+  res_msg: 'success',
   data: {
-    personInfo: {
-      partyNo: '111',
-      firstName: '111',
-      middleName: '111',
-      lastName: '111',
-      idType: '111',
-      idNo: '111',
-      sex: '111',
-      birthday: '111',
-      idExpireDate: '111',
-      mobileNo: '111',
-      email: '111',
-    },
-    employmentInfo: {
-      'job ': '111',
-      company: '111',
-      fundsource: '111',
-    },
-    taxInfo: {
-      residence: '111',
-      tin: '111',
-      lackReason: '111',
-      explanation: '1111',
-    },
-    residential: {
-      region: '111',
-      city: '111',
-      address: '111',
-      zipCode: '111',
-      identificationMethod: '111',
-
-    },
-    csa: {
-      riskProfile: '111',
-    },
-    bankInfo: {
-      referenceCode: '111',
-    },
+    totalCount: 1,
+    totalPage: 1,
+    pageIndex: 1,
+    data: [
+      {
+        partyNo: 'partyNo',
+        userName: 'Customer Name',
+        mobileNo: 'Mobile Number',
+        email: 'E-mail',
+        registerTime: 'Registration Date',
+        openAccountTime: 'Account Opening Date',
+      },
+    ],
   },
 };
